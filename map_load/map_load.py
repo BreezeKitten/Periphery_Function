fbeef5a99f6fb285326b71c687c061b4ba9086e0
BreezeCat <jkjk010695@gmail.com>
2020-02-12 14:04:50 +0800

    modify Map to Class

--- map_load/map_load.py
+++ map_load/map_load.py
@@ -3,47 +3,63 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import yaml
 
 img_name = 'testmap.png'
 
 
-def load_img(name):
-    lena = mpimg.imread(name)
-    return lena
+class Map():
+    def __init__(self, yaml_file):
+        with open(yaml_file, 'r') as f:
+            Info = yaml.load(f, Loader=yaml.FullLoader)
+        self.image = Info['image']
+        self.resolution = Info['resolution']
+        self.origin = Info['origin']
+        self.load_map()
+        
+    def load_map(self):
+        self.gray_image = np.dot(mpimg.imread(self.image)[...,:3], [0.299, 0.587, 0.114])
+        self.heigh = self.gray_image.shape[0]
+        self.width = self.gray_image.shape[1]
+        self.obs_map = [[0 for i in range(self.heigh)]
+                      for j in range(self.width)]
+        for i in range(self.heigh):
+            for j in range(self.width):
+                if self.gray_image[i][j] == 0:
+                    self.obs_map[j][self.heigh-1-i] = 1 #[x][y] = 0 is obs
+                    
+                    
+    def show_map(self):
+        figure_res = 0.05
+        plt.figure(figsize=(5,5))
+        ox, oy = [], []
     
-def rgb2gray(rgb):
-    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
-
-def show_map(image_name, map_res, origin):
-    image = load_img(image_name)
-    image_g = rgb2gray(image)
-    #plt.imshow(image_g, cmap='Greys_r', origin='lower')
-    #map_res = 1 # meter/pixel
-    figure_res = 0.05
-    plt.figure(figsize=(5,5))
-    ox, oy = [], []
-    obs_map = [[1 for i in range(image_g.shape[0])]
-                      for j in range(image_g.shape[1])]
-    
-    for i in range(image_g.shape[0]):
-        for j in range(image_g.shape[1]):
-            if image_g[i][j] == 0:
-                obs_map[j][image_g.shape[0]-1-i] = 0  #[x][y] 0 is obs
-                for k in range(int(1/figure_res)):
-                    for l in range(int(1/figure_res)):
-                        ox.append(origin[0]+(j+figure_res*k)*map_res)
-                        oy.append(origin[1]+(image_g.shape[0]-i-figure_res*l)*map_res)
+        for i in range(self.heigh):
+            for j in range(self.width):
+                if self.gray_image[i][j] == 0:
+                    for k in range(int(1/figure_res)):
+                        for l in range(int(1/figure_res)):
+                            ox.append(self.origin[0]+(j+figure_res*k)*self.resolution)
+                            oy.append(self.origin[1]+(self.heigh-i-figure_res*l)*self.resolution)
                         
-    plt.plot(ox, oy, ".k")
-    plt.grid(True)
-    plt.axis("equal")
-    plt.xlim(origin[0],origin[0]+image_g.shape[1]*map_res)
-    plt.ylim(origin[1],origin[1]+image_g.shape[0]*map_res)
-    plt.show()
+        plt.plot(ox, oy, ".k")
+        plt.grid(True)
+        plt.axis("equal")
+        plt.xlim(self.origin[0],self.origin[0]+self.width*self.resolution)
+        plt.ylim(self.origin[1],self.origin[1]+self.heigh*self.resolution)  
+        plt.show()
+        
+    def Abs2Map(self, Abs):
+        M_X = int(Abs[0]/self.resolution - self.origin[0]/self.resolution)
+        M_Y = int(Abs[1]/self.resolution - self.origin[1]/self.resolution)
+        return [M_X, M_Y]
     
-    return obs_map
+    def Map2Abs(self, M):
+        Abs_X = M[0]*self.resolution + self.origin[0]
+        Abs_Y = M[1]*self.resolution + self.origin[1]
+        return [Abs_X, Abs_Y]
 
 
 if __name__ == '__main__':
-    A = show_map(img_name, 1, [0,0])
+    A = Map('testmap.yaml')
 
